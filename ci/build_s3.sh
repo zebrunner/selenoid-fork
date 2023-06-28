7b2bea2ccc38eebcdb7f1372ce2a4b740bc470f4
azarouski <azarouski@solvd.com>
2023-06-28 17:23:12 +0300

    build_s3.sh file for compiling Zebrunner-Selenoid images was added

---
+++ ci/build_s3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ../
+mkdir -p dist
+
+export GOOS=linux
+export GOARCH=amd64
+export CGO_ENABLED=0
+
+if go build -o dist/selenoid_linux_amd64 -tags s3,metadata; then
+    echo -e "Selenoid build was successful. Building docker image:"
+    docker buildx build --pull --platform linux/amd64 -t public.ecr.aws/zebrunner/selenoid:1.10.12 .
+else
+    echo -e "Selenoid build was not successful"
+fi
+
+rm -rf dist
